3a14d53e341122dc3109449e39f4b574ae5127d4
Ericp95 <ericp6960@gmail.com>
2026-09-25 15:30:15 -0300

    agg opcion 1

--- PreEntrega.py
+++ PreEntrega.py
@@ -3,10 +3,11 @@ productos=[]
 while True :
 
     print("\n" "*****MENU*****")
+    print("\n" "1_Ingrese productos")
 
-    opciones=input("\n""Seleccione una Opcion, 1-salir")
+    opciones=input("\n""Seleccione una Opcion, 5-salir")
 
     match opciones:
-        case "1":
+        case "5":
             print("\n Gracias por usar nuestro menu")
             break
